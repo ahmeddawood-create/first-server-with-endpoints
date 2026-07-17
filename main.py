731edb63d39372a3c463463f390cdf8aa9dbd24e
Ahmed Dawood <ahmeddawood.pro@gmail.com>
2026-07-17 20:14:35 +0500

    Stage 0: hello server

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-from fastapi import FastAPI
 
-myapp = FastAPI()
-
-@myapp.get("/home")
-def home(name: str = "visitor"):
-  return {"message":f"hello {name}"}
-
-@myapp.get("/status")
-def sitestatus():
-  return {"status":"working"}
